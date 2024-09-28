da2011ea5ca36e51b4198df30181c4c59e234d79
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2024-09-28 20:53:29 +0200

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import { useFilePicker } from 'use-file-picker'
+import { parseArrayBuffer } from 'midi-json-parser'
+import { useState } from 'react'
 
 export default function Home() {
-  const { openFilePicker, filesContent, loading } = useFilePicker({
-    accept: '.txt',
+  const [midi, setMidi] = useState()
+  const { openFilePicker } = useFilePicker({
+    accept: ['.mid', '.midi'],
+    readAs: 'ArrayBuffer',
+    onFilesSelected: async ({ filesContent }) => {
+      setMidi(await parseArrayBuffer(filesContent[0].content))
+    },
   })
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Select midi file
+        <button onClick={openFilePicker}>Select midi file</button>
+        {JSON.stringify(midi, null, 4)}
       </main>
     </div>
   )
