d7f69ebd65f8e6768483094ed295ea02a7de5bb1
mgabor3141 <9047995+mgabor3141@users.noreply.github.com>
2024-10-14 14:04:13 +0200

    Fix consistency test

--- app/lib/song-to-factorio.test.ts
+++ app/lib/song-to-factorio.test.ts
@@ -1,10 +1,16 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
 import { songToFactorio, songToFactorioData } from '@/app/lib/song-to-factorio'
 import { Midi } from '@tonejs/midi'
 import { preprocessSong } from '@/app/components/select-stage'
 import { readFile } from 'node:fs/promises'
+import { mkAlea } from '@spissvinkel/alea'
 
 describe('Song to Factorio', () => {
+  beforeEach(() => {
+    const { random } = mkAlea('seed')
+    vi.spyOn(Math, 'random').mockImplementation(() => random())
+  })
+
   test('consistency', async () => {
     const file = await readFile('test-data/debussy-clair-de-lune.mid')
 
