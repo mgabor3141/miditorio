24143294e4ea2fe5def88cc4599b71cc21cfa71d
mgabor <9047995+mgabor3141@users.noreply.github.com>
2024-10-18 15:43:14 +0200

    add tests for note conversion

--- app/lib/factorio-instrument.test.ts
+++ app/lib/factorio-instrument.test.ts
@@ -1,55 +1,68 @@
-import { expect, test } from 'vitest'
-import { Frequency } from 'tone'
+import { describe, expect, test } from 'vitest'
+import { Frequency, isArray } from 'tone'
 import { getFactorioInstrument } from '@/app/lib/factorio-instrument'
+import type { FactorioInstrumentName } from '@/app/lib/data/factorio-instruments-by-id'
+import type { Note } from 'tone/build/esm/core/type/Units'
+import { MidiNote } from 'tone/build/esm/core/type/NoteUnits'
 
-test('Piano too low note', () => {
-  const piano = getFactorioInstrument('Piano')
-  const E2 = Frequency('E2').toMidi()
+const note = (noteString: Note) => [Frequency(noteString).toMidi(), noteString]
 
-  expect(
-    piano.noteToFactorioNote(
-      E2,
-      { octaveShift: 0, velocityValues: [], factorioInstruments: ['Piano'] },
-      { globalNoteShift: 0, speedMultiplier: 1 },
-    ),
-  ).toBe({ valid: false, outOfRangeDirection: 'lower' })
-})
+// T is a single test case parameter, or an array that needs to be unwrapped
+const cartesianProduct = <T>(...arrays: T[][]): T[][] =>
+  arrays.reduce(
+    (acc, curr) =>
+      acc.flatMap((a) => curr.map((b) => [...a, ...(isArray(b) ? b : [b])])),
+    [[]] as T[][],
+  )
 
-test('Piano lowest note', () => {
-  const piano = getFactorioInstrument('Piano')
-  const F2 = Frequency('F2').toMidi()
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const testCases = cartesianProduct<any>(
+  // octave shifts
+  [0, 1, 2, -1],
+  // global shifts
+  [0, 1, 2, -1],
+  // Notes
+  [
+    note('E2'),
+    note('F2'),
+    note('E4'),
+    note('F4'),
+    note('E6'),
+    note('E#6'),
+    note('E7'),
+    note('E#7'),
+  ],
+  // Instruments
+  ['Piano', 'Celesta'],
+) as [number, number, MidiNote, Note, FactorioInstrumentName][]
 
-  expect(
-    piano.noteToFactorioNote(
-      F2,
-      { octaveShift: 0, velocityValues: [], factorioInstruments: ['Piano'] },
-      { globalNoteShift: 0, speedMultiplier: 1 },
-    ),
-  ).toBe({ valid: true, factorioNote: 1 })
-})
+describe('Note conversion', () => {
+  test.for(testCases)(
+    '%d octaveShift, %d noteShift, note %d (%s) %s',
+    ([octaveShift, globalNoteShift, note, _noteName, instrumentName]) => {
+      const instrument = getFactorioInstrument(
+        instrumentName as FactorioInstrumentName,
+      )
 
-test('Piano highest note', () => {
-  const piano = getFactorioInstrument('Piano')
-  const E6 = Frequency('E6').toMidi()
+      const result = instrument.noteToFactorioNote(
+        note,
+        {
+          octaveShift,
+          velocityValues: [],
+          factorioInstruments: [instrumentName],
+        },
+        { globalNoteShift, speedMultiplier: 1 },
+      )
 
-  expect(
-    piano.noteToFactorioNote(
-      E6,
-      { octaveShift: 0, velocityValues: [], factorioInstruments: ['Piano'] },
-      { globalNoteShift: 0, speedMultiplier: 1 },
-    ),
-  ).toBe({ valid: true, factorioNote: 48 })
-})
+      expect(result).toSatisfy(
+        (result: Record<string, unknown>) =>
+          !!(
+            (result.valid && (result.factorioNote as number) > 0) ||
+            (!result.valid && result.outOfRangeDirection)
+          ),
+      )
 
-test('Piano too high note', () => {
-  const piano = getFactorioInstrument('Piano')
-  const ESharp6 = Frequency('E#6').toMidi()
-
-  expect(
-    piano.noteToFactorioNote(
-      ESharp6,
-      { octaveShift: 0, velocityValues: [], factorioInstruments: ['Piano'] },
-      { globalNoteShift: 0, speedMultiplier: 1 },
-    ),
-  ).toBe({ valid: false, outOfRangeDirection: 'higher' })
+      expect(result).toMatchSnapshot()
+    },
+  )
 })
